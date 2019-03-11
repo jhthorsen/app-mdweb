9f1205903b99a9badba3f519ef94c5ecfc7d84ab
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-03-11 10:21:22 +0800

    Will serve files from current directory

--- cpanfile
+++ cpanfile
@@ -1,6 +1,5 @@
 # You can install this projct with curl -L http://cpanmin.us | perl - https://github.com/jhthorsen/app-mdweb/archive/master.tar.gz
 requires "Mojolicious"         => "8.00";
-requires "File::HomeDir"       => "1.00";
 requires "Text::MultiMarkdown" => "1.00";
 
 test_requires "Test::More" => "0.88";
--- lib/App/mdweb.pm
+++ lib/App/mdweb.pm
@@ -16,10 +16,14 @@ App::mdweb - Markdown server and file browser
   $ mdweb daemon
   $ mdweb daemon --listen http://127.0.0.1:5000
   $ mdweb daemon --listen http://*:8080
+  $ MDWEB_HOME=$HOME/Documents/markdown mdweb daemon
 
 =head1 DESCRIPTION
 
 This webserver can act as a file browser and markdown renderer.
+
+It defaults to serving documents from the current directory, but this can be
+changed with the environment variable C<MDWEB_HOME>.
 
 =head1 AUTHOR
 
@@ -27,7 +31,7 @@ Jan Henning Thorsen
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2014, Jan Henning Thorsen
+Copyright (C) Jan Henning Thorsen
 
 This program is free software, you can redistribute it and/or modify it under
 the terms of the Artistic License version 2.0.
